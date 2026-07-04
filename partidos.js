9aea1f80d7191ab817e227aae6d0c02ee0ad125b
FedeReloaded <48686982+fedeRealoded@users.noreply.github.com>
2026-07-04 11:03:36 -0300

    Update partidos.js

--- partidos.js
+++ partidos.js
@@ -1,71 +1,22 @@
 const PARTIDOS = [
   {
-    "id": 73,
-    "fecha": "2026-06-28",
-    "grupo": "16avos de final",
-    "equipo1": "Sudáfrica",
-    "equipo2": "Canadá",
-    "senal": "Dsports",
-    "canales": [
-      "Dsports"
-    ],
-    "hora": "16:00"
-  },
-  {
-    "id": 74,
-    "fecha": "2026-06-29",
-    "grupo": "16avos de final",
-    "equipo1": "Brasil",
-    "equipo2": "Japón",
-    "senal": "Dsports",
-    "canales": [
-      "Dsports"
-    ],
-    "hora": "14:00"
-  },
-  {
-    "id": 75,
-    "fecha": "2026-06-29",
-    "grupo": "16avos de final",
-    "equipo1": "Alemania",
-    "equipo2": "Paraguay",
-    "senal": "Dsports",
-    "canales": [
-      "Dsports",
-      "TyC Sports"
-    ],
-    "hora": "17:30"
-  },
-  {
-    "id": 76,
-    "fecha": "2026-06-29",
-    "grupo": "16avos de final",
-    "equipo1": "Holanda",
+    "id": 89,
+    "fecha": "2026-07-04",
+    "grupo": "Octavos de final",
+    "equipo1": "Canadá",
     "equipo2": "Marruecos",
     "senal": "Dsports",
     "canales": [
       "Dsports"
     ],
-    "hora": "22:00"
-  },
-  {
-    "id": 77,
-    "fecha": "2026-06-30",
-    "grupo": "16avos de final",
-    "equipo1": "Costa de Marfil",
-    "equipo2": "Noruega",
-    "senal": "Dsports",
-    "canales": [
-      "Dsports"
-    ],
     "hora": "14:00"
   },
   {
-    "id": 78,
-    "fecha": "2026-06-30",
-    "grupo": "16avos de final",
-    "equipo1": "Francia",
-    "equipo2": "Suecia",
+    "id": 90,
+    "fecha": "2026-07-04",
+    "grupo": "Octavos de final",
+    "equipo1": "Paraguay",
+    "equipo2": "Francia",
     "senal": "Dsports",
     "canales": [
       "Dsports",
@@ -74,47 +25,48 @@ const PARTIDOS = [
     "hora": "18:00"
   },
   {
-    "id": 79,
-    "fecha": "2026-06-30",
-    "grupo": "16avos de final",
-    "equipo1": "México",
-    "equipo2": "Ecuador",
+    "id": 91,
+    "fecha": "2026-07-05",
+    "grupo": "Octavos de final",
+    "equipo1": "Brasil",
+    "equipo2": "Noruega",
     "senal": "Dsports",
     "canales": [
-      "Dsports"
-    ],
-    "hora": "22:00"
-  },
-  {
-    "id": 80,
-    "fecha": "2026-07-01",
-    "grupo": "16avos de final",
-    "equipo1": "Inglaterra",
-    "equipo2": "RD Congo",
-    "senal": "Dsports",
-    "canales": [
-      "Dsports"
-    ],
-    "hora": "13:00"
-  },
-  {
-    "id": 81,
-    "fecha": "2026-07-01",
-    "grupo": "16avos de final",
-    "equipo1": "Bélgica",
-    "equipo2": "Senegal",
-    "senal": "Dsports",
-    "canales": [
-      "Dsports"
+      "Dsports",
+      "TyC Sports"
     ],
     "hora": "17:00"
   },
   {
-    "id": 82,
-    "fecha": "2026-07-01",
-    "grupo": "16avos de final",
+    "id": 92,
+    "fecha": "2026-07-05",
+    "grupo": "Octavos de final",
+    "equipo1": "México",
+    "equipo2": "Inglaterra",
+    "senal": "Dsports",
+    "canales": [
+      "Dsports"
+    ],
+    "hora": "21:00"
+  },
+  {
+    "id": 93,
+    "fecha": "2026-07-06",
+    "grupo": "Octavos de final",
+    "equipo1": "Portugal",
+    "equipo2": "España",
+    "senal": "Dsports",
+    "canales": [
+      "Dsports"
+    ],
+    "hora": "16:00"
+  },
+  {
+    "id": 94,
+    "fecha": "2026-07-06",
+    "grupo": "Octavos de final",
     "equipo1": "USA",
-    "equipo2": "Bosnia",
+    "equipo2": "Bélgica",
     "senal": "Dsports",
     "canales": [
       "Dsports",
@@ -123,80 +75,29 @@ const PARTIDOS = [
     "hora": "21:00"
   },
   {
-    "id": 83,
-    "fecha": "2026-07-02",
-    "grupo": "16avos de final",
-    "equipo1": "España",
-    "equipo2": "Austria",
-    "senal": "Dsports",
-    "canales": [
-      "Dsports"
-    ],
-    "hora": "16:00"
-  },
-  {
-    "id": 84,
-    "fecha": "2026-07-02",
-    "grupo": "16avos de final",
-    "equipo1": "Portugal",
-    "equipo2": "Croacia",
-    "senal": "Dsports",
-    "canales": [
-      "Dsports",
-      "TyC Sports",
-      "Telefe"
-    ],
-    "hora": "20:00"
-  },
-  {
-    "id": 85,
-    "fecha": "2026-07-03",
-    "grupo": "16avos de final",
-    "equipo1": "Suiza",
-    "equipo2": "Argelia",
-    "senal": "Dsports",
-    "canales": [
-      "Dsports"
-    ],
-    "hora": "00:00"
-  },
-  {
-    "id": 86,
-    "fecha": "2026-07-03",
-    "grupo": "16avos de final",
-    "equipo1": "Australia",
-    "equipo2": "Egipto",
-    "senal": "Dsports",
-    "canales": [
-      "Dsports",
-      "TyC Sports"
-    ],
-    "hora": "15:00"
-  },
-  {
-    "id": 87,
-    "fecha": "2026-07-03",
-    "grupo": "16avos de final",
+    "id": 95,
+    "fecha": "2026-07-07",
+    "grupo": "Octavos de final",
     "equipo1": "Argentina",
-    "equipo2": "Cabo Verde",
+    "equipo2": "Egipto",
     "senal": "TyC Sports",
     "canales": [
       "Dsports",
       "TyC Sports",
       "Telefe"
     ],
-    "hora": "19:00"
+    "hora": "13:00"
   },
   {
-    "id": 88,
-    "fecha": "2026-07-03",
-    "grupo": "16avos de final",
-    "equipo1": "Colombia",
-    "equipo2": "Ghana",
+    "id": 96,
+    "fecha": "2026-07-07",
+    "grupo": "Octavos de final",
+    "equipo1": "Suiza",
+    "equipo2": "Colombia",
     "senal": "Dsports",
     "canales": [
       "Dsports"
     ],
-    "hora": "22:30"
+    "hora": "16:00"
   }
 ];
